f481c7a3dbe5a2ec5f16c0801232ed0d9f034af9
FaigBagirov <bagirov3105@gmail.com>
2021-11-11 17:07:24 +0400

    krono boilerplate

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+import "./css/style.css"
+
+
+//should stay at the end of the file
+if (module.hot){
+    module.hot.accept();
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/img", to: "img" },
+      ],
+    }),
+  ],
+
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+      port: 80,
+      static: path.resolve(__dirname, 'dist'),
+      hot: true
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  }
+};
