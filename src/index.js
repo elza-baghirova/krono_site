f84f03702f379a2380993058e38f04f70b9cc3aa
FaigBagirov <bagirov3105@gmail.com>
2021-11-17 15:21:25 +0400

    sorting ready but no transition

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // import _ from 'lodash';
 import "./css/style.css"
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     var nav = document.getElementById("myNav"),
-    scrollDetector = document.getElementById("scroll-start-detector");
-    
+        scrollDetector = document.getElementById("scroll-start-detector");
+
     console.log("nav: ", nav);
     console.log("scrollDetector: ", scrollDetector);
 
     //function handles callback when observer fires "observing/notObserving" events
-    function handleIntersection(entriesArray){
+    function handleIntersection(entriesArray) {
         entriesArray.map((entry) => {
             if (entry.isIntersecting) {
                 // nav.classList.remove('visible');
@@ -22,40 +22,68 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 // nav.style.backgroundColor = "rgb(5, 55, 123, 0.4)";
                 nav.style.backgroundColor = "rgb(4, 28, 40, 0.5)";
             }
-          });
+        });
     }
     //creates new observer with our callback func passed as an argument
     const observer = new IntersectionObserver(handleIntersection);
-    
+
     //starts observing specified element
     observer.observe(scrollDetector);
 
     let seeMore = document.querySelector(".products-heading__more"),
-    moreProductNavs = document.querySelector(".more-product-navs");
+        moreProductNavs = document.querySelector(".more-product-navs");
 
-    seeMore.addEventListener('click', ()=>{
+    seeMore.addEventListener('click', () => {
         seeMore.classList.add('hide');
         moreProductNavs.classList.remove('hide');
     });
-    
-    let mainSandwich = document.querySelector(".main-sandwich"),
-    mainMenu = document.querySelector(".main-menu"),
-    sandwichLine = document.querySelector(".sandwich-line"),
-    sandwichX = document.querySelector(".main-sandwich-x");
 
-    mainSandwich.addEventListener('click', ()=>{
+    let mainSandwich = document.querySelector(".main-sandwich"),
+        mainMenu = document.querySelector(".main-menu"),
+        sandwichLine = document.querySelector(".sandwich-line"),
+        sandwichX = document.querySelector(".main-sandwich-x");
+
+    mainSandwich.addEventListener('click', () => {
         mainMenu.classList.add('menu-active');
         mainSandwich.style.display = 'none';
         sandwichX.style.display = 'block';
     });
 
-    sandwichX.addEventListener('click', ()=>{
+    sandwichX.addEventListener('click', () => {
         mainMenu.classList.remove('menu-active');
         mainSandwich.style.display = 'block';
         sandwichX.style.display = 'none';
     });
 
+    let gallerySortingNames = document.querySelectorAll(".gallery-sorting__name");
+    gallerySortingNames.forEach((element, i) => {
+        element.addEventListener("click", () => {
+            if (element.dataset.name === "js-hamisi"){
+                for (let n = 0; n < gallerySortingNames.length; n++) {                
+                    console.log(`show all dataset-${i}`, element.dataset.name);
+                    let sorted = document.querySelectorAll(".product-card__size__sm");
+                    // sorted.forEach((item)=>{item.style.display = "block"});                
+                }
+            }
+            else {
+                for (let n = 0; n < gallerySortingNames.length; n++) {                
+                    if (n === i) {
+                        let sorted = document.querySelectorAll("." + element.dataset.name);
+                        // sorted.forEach((item)=>{item.style.display = "block"});
+                    }
+                    else {
+                        let unSorted = document.querySelectorAll("." + gallerySortingNames[n].dataset.name);
+                        
+                        unSorted.forEach((item)=>{item.classList.add("gallery-hide")});
+                        setTimeout(function() {
+                            unSorted.forEach((item)=>{item.style.display = "none"});
+                          }, 2000);
+                    }
+                }
+            }
+        });
 
+    });
 
 
 
@@ -68,6 +96,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
 });
 
 //should stay at the end of the file
-if (module.hot){
+if (module.hot) {
     module.hot.accept();
 }
